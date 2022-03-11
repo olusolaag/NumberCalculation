4ad5d2f21b425db2ae0c0eb1a24b5d362c645189
olusolaag <olusolaagboola10@gmail.com>
2022-03-11 09:49:39 -0500

    test cases modified

--- src/test/java/com/demo/NumberCalculationTest.java
+++ src/test/java/com/demo/NumberCalculationTest.java
@@ -13,16 +13,6 @@ public class NumberCalculationTest {
 	NumberCalculation nc = new NumberCalculation();
 	
 	@Test
-	public void testCase1() {
-		assertEquals(nc.add(1, 2), 100);
-	}
-	
-	@Test
-	public void testCase2() {
-		assertEquals(nc.add(1, 2), 0);
-	}
-	
-	@Test
 	public void testCase3() {
 		assertEquals(nc.add(1, 2), 3);
 	}
